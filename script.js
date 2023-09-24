37a4c9a7042043d64a58d53f76f7edbc9dd208d5
gabicerqueira <133364342+gabicerqueira@users.noreply.github.com>
2023-09-24 14:33:13 -0300

    Adição
    Adicionei salvar desenho e alternancia entre borracha e lapis

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ let corSelecionada = "#000000";
 let modoBorracha = false;
 let formas = [];
 let acoes = []; // Armazena ações de desenho para desfazer
+let formaSelecionada = "lapis"; // Inicialmente, a forma padrão é lápis
+document.getElementById("botaoLapis").classList.add("selecionado");
+
+document.getElementById("botaoLapis").addEventListener("click", function () {
+    formaSelecionada = "lapis";
+
+    tamanhoSlider.value = 1;//tamanho do lápis
+    contexto.lineWidth = tamanhoSlider.value; //base no valor do slider
+    modoBorracha = false;
+
+    document.getElementById("botaoBorracha").classList.remove("selecionado");
+    this.classList.add("selecionado"); // Adicione a classe "selecionado" quando o lápis for selecionado
+
+});
 
 let tamanhoSlider = document.getElementById("tamanhoDoTraco");
 
@@ -33,6 +47,8 @@ canvas.addEventListener("mousemove", function (event) {
         contexto.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop); //o lineTo() vai juntar as coordenadas e identificar a linha que estamos traçando enquanto clicamos e movemos o mouse
         contexto.stroke(); //traça a linha
     }
+
+
 });
 
 canvas.addEventListener("mouseup", function (event) {
@@ -53,21 +69,22 @@ document.getElementById("seletorDeCores").addEventListener("input", function () 
 });
 
 document.getElementById("botaoBorracha").addEventListener("click", function () {
+    formaSelecionada = "borracha"; // Defina a forma selecionada como borracha
 
     if (modoBorracha) {
-        tamanhoSlider.value = 1;//tamanho do lápis
-        contexto.lineWidth = tamanhoSlider.value; //base no valor do slider
-        this.style.backgroundColor = ""; // Limpa o estilo de fundo
-        this.style.color = '#000'
+        tamanhoSlider.value = 1; // tamanho do lápis
+        contexto.lineWidth = tamanhoSlider.value; // base no valor do slider
+        this.classList.remove("selecionado");
     } else {
-        tamanhoSlider.value = 10; //tamanho da borracha
-        contexto.lineWidth = tamanhoSlider.value; //base no valor do slider
-        this.style.backgroundColor = "#828282"; // Define o estilo de fundo para indicar que a borracha está ativada
-        this.style.color = '#fff'
+        tamanhoSlider.value = 10; // tamanho da borracha
+        contexto.lineWidth = tamanhoSlider.value; // base no valor do slider
+        this.classList.add("selecionado");
+        document.getElementById("botaoLapis").classList.remove("selecionado");
     }
 
     modoBorracha = !modoBorracha; // Alternar entre lápis e borracha
 });
+
 
 document.getElementById("botaoPreencher").addEventListener("click", function () {
     // Preenche a última forma desenhada com a cor selecionada
@@ -91,8 +108,32 @@ document.getElementById("botaoDesfazer").addEventListener("click", function () {
             contexto.putImageData(acoes[acoes.length - 1], 0, 0); // Restaura a ação anterior
         }
     }
+    formas.pop(); // Remove a última forma desenhada
 });
 
 document.getElementById("botaoLimpar").addEventListener("click", function () {
     contexto.clearRect(0, 0, canvas.width, canvas.height); // Limpa o canvas
 });
+
+document.getElementById("salvarDesenho").addEventListener("click", function () {
+    salvarCanvasComoImagem();
+});
+
+function salvarCanvasComoImagem() {
+    let canvasCopia = document.createElement('canvas');
+    let contextoCopia = canvasCopia.getContext('2d');
+
+    canvasCopia.width = canvas.width;
+    canvasCopia.height = canvas.height;
+
+    contextoCopia.fillStyle = "#ffffff"; // Preenche o novo canvas com o fundo branco
+    contextoCopia.fillRect(0, 0, canvasCopia.width, canvasCopia.height);
+
+    contextoCopia.drawImage(canvas, 0, 0); // Desenha o conteúdo do canvas original na cópia
+
+    let link = document.createElement('a');
+    link.href = canvasCopia.toDataURL(); // Converte o conteúdo da cópia em uma URL de dados
+
+    link.download = 'desenho.png';
+    link.click();
+}
